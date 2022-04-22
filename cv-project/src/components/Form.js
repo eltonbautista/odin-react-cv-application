06fd14d0cc283b3d4d2abd73c508542959f497f9
Elton Bautista <eltonbestaha@gmail.com>
2022-04-22 13:58:13 -0600

    Form component responsible for form half of CV

--- cv-project/src/components/Form.js
+++ cv-project/src/components/Form.js
@@ -1,1 +1,34 @@
 // This component will be used as a container for my form(s)
+import React, { Component } from 'react';
+// import App from '../App';
+import FormSubComponents from './FormSubComponents';
+
+// Okay so we have these FormSubComponents, then each one of them will have specific attributes.
+// Each attribute will create a new <li>{attribute}</li> inside of them (through the FormSubComponent.js)
+// Example: <FormSubComponents title='Personal Information' attrOne='Name' attrTwo='Email' attrThree='Phone number' />
+// This will then become an HTML that looks like this:
+/* <p>Personal Information</p>
+	<ul>
+		<li>
+			<label>{attrOne (Name:)}</label> <input></input>
+		</li>
+		<li>
+			<label>{attrTwo (Email:)}</label> <input></input>
+		</li>
+	</ul>
+*/
+// Right, so by adding attributes it will increase the amount of items in the descriptors array.
+
+class Form extends Component {
+	render() {
+		return (
+			<div>
+				<FormSubComponents title='Personal Information' />
+				<FormSubComponents title='Education' />
+				<FormSubComponents title='Work History' />
+			</div>
+		);
+	}
+}
+
+export default Form;
