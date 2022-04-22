16236cb9590ae89cfc6bab5c73a82c8b57e605ee
Elton Bautista <eltonbestaha@gmail.com>
2022-04-22 13:57:41 -0600

    App.js is properly rendering component(s)

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import React, { Component } from 'react';
-
+import Form from './components/Form';
 // function App() {
 // 	return <div className='App'></div>;
 // }
 
 class App extends Component {
 	render() {
-		return <div className='App'></div>;
+		// eslint-disable-next-line prettier/prettier
+		return (
+			<div className='App'>
+				<Form />
+			</div>
+		);
 	}
 }
 export default App;
