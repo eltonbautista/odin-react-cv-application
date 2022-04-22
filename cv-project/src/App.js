ccdcaa6b4db187d672c305710b3d48eddcdc1233
Elton Bautista <eltonbestaha@gmail.com>
2022-04-22 11:02:27 -0600

    Checking if adding an excalidraw file will cause issues

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-	return <div className='App'></div>;
+// function App() {
+// 	return <div className='App'></div>;
+// }
+
+class App extends Component {
+	render() {
+		return <div className='App'></div>;
+	}
 }
-
 export default App;
