fe68e62adff3b17b35d899a92f84c1da6437df4b
Elton Bautista <eltonbestaha@gmail.com>
2022-04-25 16:18:31 -0600

    Okay so I worked on a bit of the logic for ResumePreview components They are very similar to the Form components, I'm wondering if there's a way to make them DRY..

---
+++ cv-project/src/components/ResumePreviewHeaderRows.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ResumePreviewRows from './ResumePreviewRows';
+
+class ResumePreviewHeaderRows extends Component {
+	constructor({ title, information }) {
+		super();
+
+		this.title = title;
+		this.information = information;
+	}
+
+	render() {
+		const title = this.title;
+		const information = this.information;
+
+		return (
+			<div>
+				<p>{title}</p>
+				<ResumePreviewRows information={information} />
+			</div>
+		);
+	}
+}
--- cv-project/src/components/ResumePreviewRows.js
+++ cv-project/src/components/ResumePreviewRows.js
@@ -4,25 +4,27 @@ class ResumePreviewRows extends Component {
 	constructor({ information }) {
 		super();
 
-        this.information = information;
+		this.information = information;
 	}
 
 	render() {
-        const information = this.information;
+		const information = this.information;
 
-        const listOfInformation = information.map((info) => {
-            const properId = info.toLowerCase().replaceAll(' ', '-') + '-preview';
+		const listOfInformation = information.map((info) => {
+			const properId = info.toLowerCase().replaceAll(' ', '-') + '-preview';
 
-            return(
-                <li key={properId}>
-                    <div>{info}</div>
-                </li>
-            )
-        })
+			return (
+				<li key={properId}>
+					<div>{info}</div>
+				</li>
+			);
+		});
 
-		return <ul>
-            {listOfInformation}
-        </ul>;
+		return (
+			<div>
+				<ul>{listOfInformation}</ul>
+			</div>
+		);
 	}
 }
-}
+export default ResumePreviewRows;
