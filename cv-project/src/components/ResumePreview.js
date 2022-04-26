9ecef688febe98a21660c863af7985b19deac240
Elton Bautista <eltonbestaha@gmail.com>
2022-04-26 09:35:34 -0600

    Added the content for the static version of ResumePreview

--- cv-project/src/components/ResumePreview.js
+++ cv-project/src/components/ResumePreview.js
@@ -16,8 +16,40 @@ class ResumePreview extends Component {
 		return (
 			<div id='resume-preview'>
 				<ResumePreviewHeaderRows
-					information={['Name', 'Email', 'Phone number']}
+					title='Personal Information'
+					information={[
+						'Name',
+						'Title',
+						'Address',
+						'Email',
+						'Phone number',
+						'Description',
+					]}
 					personalImage={profileImage}
+				/>
+				<ResumePreviewHeaderRows
+					title='Work Experience'
+					information={['Position', 'Company', 'City']}
+				/>
+				<ResumePreviewHeaderRows
+					title='Education'
+					information={['University name', 'City', 'Degree']}
+				/>
+				<ResumePreviewHeaderRows
+					title='Certification'
+					information={['Certification Example']}
+				/>
+				<ResumePreviewHeaderRows
+					title='Tech Stack'
+					information={['CSS', 'HTML']}
+				/>
+				<ResumePreviewHeaderRows
+					title='Skills'
+					information={['Customer service']}
+				/>
+				<ResumePreviewHeaderRows
+					title='Passions'
+					information={['Snowboarding', 'Bouldering']}
 				/>
 			</div>
 		);
